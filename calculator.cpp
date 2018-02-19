dac11a632b9e80682b0c28538b9d5c91f5bceae8
William Horn <wbhorn@alaska.edu>
2018-02-18 20:09:11 -0900

    Delimiter can be changed

--- calculator.cpp
+++ calculator.cpp
@@ -3,15 +3,28 @@
 
 #include "calculator.h"
 
+#include <iostream>
+using std::cout;
+using std::endl;
 #include <string>
 using std::string;
 #include <sstream>
 using std::istringstream;
 
+static char getDelimiterFrom(const string & numbers) {
+    if (numbers.size() > 3 && numbers[0] == '/' && numbers[1] == '/') {
+        return numbers[2];
+    }
+
+    return ',';
+}
+
 int add(string numbers)
 {
+    auto delimiter = getDelimiterFrom(numbers);
+
     for (auto i = 0; i < numbers.size(); ++i) {
-        if (numbers[i] == ',') {
+        if (numbers[i] == delimiter) {
             if (numbers[i + 1] > numbers.size() and numbers[i + 1] == '\n') {
                 return -1;
             }
